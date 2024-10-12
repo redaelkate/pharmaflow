cde8f90992f74ece166386be905edfba035831e6
Hamza-Bouali <176033415+Hamza-Bouali@users.noreply.github.com>
2024-10-12 20:16:36 +0000

    Pages layout

--- pharmaflow/src/componements/navbar/nav.jsx
+++ pharmaflow/src/componements/navbar/nav.jsx
@@ -34,18 +34,15 @@ const Nav= ({setPage}) => {
                         </ul>
                     </div>
                     <div className="order-2 md:order-3">
-                        {login ? (
-                            <button onClick={setLogin(!login)} className="px-4 py-2 bg-sky-500/75 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                         
+                            {login && <button onClick={()=>{setLogin(false)}} className="px-4 py-2 bg-sky-500/75 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
                                 </svg>
                                 <span>Login</span>
-                            </button>
-                        ) : (
-                            <a onClick={() => { setLogin(!login) }}>
-                                <img src={man} width={50} style={{ border: '2px solid black' ,borderRadius:'120%', borderColor:'text-indigo-500',cursor:'pointer'}} />
-                            </a>
-                        )}
+                            </button>}
+                            {!login && <img onClick={()=>{setLogin(true)}} src={man} width={50} style={{borderColor:'black',borderWidth:'2px',borderRadius:'100px'}}/>}
+                        
                     </div>
                 </div>
             </nav>
